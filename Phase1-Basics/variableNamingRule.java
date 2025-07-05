3612cd5009710cc94fbacd9cf3b39c0cea269af9
vanshika <vanshikamehra131@gmail.com>
2025-07-05 12:01:02 +0530

    Add Var Naming Rule

---
+++ Phase1-Basics/variableNamingRule.java
@@ -0,0 +1,13 @@
+public class variableNamingRule {
+    public static void main(String[] args) {
+        
+        //! # % ^ & * ( ) - + = { } [ ] " " ; : < > , . -No allowed
+        // _ $ -Allowed
+
+        int Int=6;
+        int Else=0;
+        float _2000;
+        double _heyyy_this_is_pretty_cool;
+    }
+    
+}
