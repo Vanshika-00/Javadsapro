1cbc4c49a8b5fb1e4c6782684b86261a1f72d29a
vanshika <vanshikamehra131@gmail.com>
2025-07-05 16:03:15 +0530

    Add modulus operator

---
+++ Phase1-Basics/modulusOperator.java
@@ -0,0 +1,17 @@
+public class modulusOperator{
+ public static void main(String[] args) {
+    
+    int a=8;
+    int b=2;
+
+    System.out.println(a%b);
+    System.out.println(3%58);
+    System.out.println(28%28);
+    System.out.println(28%28);
+    System.out.println(37%10);
+    System.out.println(37%-10);
+    System.out.println(-37%10);
+    System.out.println(-37%-10);
+
+ }
+}
