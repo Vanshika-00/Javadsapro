a800efb81386b47f20790d832d9be1a75e9c0f82
vanshika <vanshikamehra131@gmail.com>
2025-07-05 16:03:55 +0530

    Add typecasting

---
+++ Phase1-Basics/halfOfGivenInt.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class halfOfGivenInt {
+    public static void main(String[] args) {
+
+        Scanner scan=new Scanner(System.in);
+        int x=scan.nextInt();
+
+        double y = (double)x;
+        System.out.println(y/2);
+
+        scan.close();
+        
+    }
+    
+}
