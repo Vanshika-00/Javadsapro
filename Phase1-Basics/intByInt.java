c9fc54757030ba8b5c36b171b56007ada6393016
vanshika <vanshikamehra131@gmail.com>
2025-07-09 15:20:12 +0530

    add typecasting-2

---
+++ Phase1-Basics/intByInt.java
@@ -0,0 +1,17 @@
+public class intByInt {
+    public static void main(String[] args) {
+        double x=5;
+        double y=2;
+        System.out.println(x/y);
+
+        double z=5/2;
+        System.out.println(z);
+
+        System.out.println(5/2);
+        System.out.println(5.0/2.0);
+        System.out.println(5.0/2);
+        System.out.println(5/2.0);
+
+    }
+    
+}
