31fb0b357a91c6426e4075cb3265d40c72027603
vanshika <vanshikamehra131@gmail.com>
2025-07-09 15:27:22 +0530

    add char dt

---
+++ Phase1-Basics/charDataType.java
@@ -0,0 +1,12 @@
+public class charDataType {
+    public static void main(String[] args) {
+        //a b c ....z
+        //A B C ....Z
+        //! @ # $ % ^ ...
+
+        char ch='8';
+        System.out.println(ch);
+        System.out.println(8);
+    }
+    
+}
