d19c0d6b3ed76c0c0f1dc48408534dd28be6b8e8
vanshika <vanshikamehra131@gmail.com>
2025-07-11 11:01:32 +0530

    add Relational Operator

---
+++ Phase2-Conditionals/Conditional/relationalOperator.java
@@ -0,0 +1,18 @@
+package Conditional;
+
+public class relationalOperator {
+
+     public static void main(String[] args) {
+        int a=8;
+        int b=12;
+
+        System.out.println(a==b);//f
+        System.out.println(a!=b);//t
+        System.out.println(a>b);//f
+        System.out.println(a<b);//t
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+     }
+
+    
+}
