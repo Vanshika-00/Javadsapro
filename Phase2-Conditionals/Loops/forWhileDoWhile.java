07535c9d9d768cdf3d518b6bf10213950576b4df
vanshika <vanshikamehra131@gmail.com>
2025-07-16 21:34:32 +0530

    for , while , do while

---
+++ Phase2-Conditionals/Loops/forWhileDoWhile.java
@@ -0,0 +1,22 @@
+public class forWhileDoWhile {
+
+    public static void main(String[] args) {
+        
+        //  for( int i=1 ; i<=4; i++){
+        //     System.out.println(i);
+        // }
+        //  int i=1;
+        //  while(i<=8){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        int i =11;
+        do{
+
+           System.out.println(i);
+           i++;
+        } while(i<11);
+    }
+    
+}
