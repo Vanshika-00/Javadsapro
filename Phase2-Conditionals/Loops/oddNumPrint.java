8c6d648735dfb3f566243809c45e5452ceec68ff
vanshika <vanshikamehra131@gmail.com>
2025-07-16 21:35:01 +0530

    odd num

---
+++ Phase2-Conditionals/Loops/oddNumPrint.java
@@ -0,0 +1,14 @@
+public class oddNumPrint {
+
+    public static void main(String[] args) {
+        
+        int i=1;
+        for(  ; i<=100 ; i++){
+            if(i%2==0) continue;
+            System.out.print( i + " ");
+        }
+        
+        
+    }
+    
+}
