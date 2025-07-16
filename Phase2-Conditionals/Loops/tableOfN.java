c99b11de658f32cd4466b445726e26a84e1d257a
vanshika <vanshikamehra131@gmail.com>
2025-07-16 11:13:08 +0530

    add Table of N

---
+++ Phase2-Conditionals/Loops/tableOfN.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class tableOfN {
+
+    public static void main(String[] args) {
+        
+         Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt(); 
+
+        for(int i=n; i<=n*10;i+=n){
+            System.out.println(i);
+        }
+
+        sc.close();
+    }
+
+
+    
+}
