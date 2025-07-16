ba2f690f7808d0bba85b9e7a1cbe5b0ad0c22184
vanshika <vanshikamehra131@gmail.com>
2025-07-16 08:54:20 +0530

    add How Loop Works

---
+++ Phase2-Conditionals/Loops/howLoopWOrks.java
@@ -0,0 +1,11 @@
+public class howLoopWOrks {
+    public static void main(String[] args) {
+        
+        int i;
+        for( i=1 ; i<=4; i++){
+            System.out.println("Hieee");
+        }
+        System.out.println(i);
+    }
+    
+}
