f8162ea80528cb6c272ca619609d82881c6b2093
vanshika <vanshikamehra131@gmail.com>
2025-07-29 04:26:13 +0530

    add Star Pyramid

--- Phase2-Conditionals/Composite Patterns/StarPyramid.java
+++ Phase2-Conditionals/Composite Patterns/StarPyramid.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 
 public class StarPyramid {
+
     public static void main(String[] args) {
         
     
      Scanner sc = new Scanner(System.in);
         System.out.printf("Enter n: ");
         int n = sc.nextInt();
+
 
         for(int i =1;i<=n;i++){
             for(int j=1;j<=n-i;j++){
@@ -15,11 +17,12 @@ public class StarPyramid {
             for(int j=1;j<=2*i-1;j++){
                 System.out.print( "*" + " ");
             }
+           
             System.out.println();
+        
         }
         sc.close();  
-    }
     
-
     
+}
 }
