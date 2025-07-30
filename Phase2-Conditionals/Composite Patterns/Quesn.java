132d717e2a606c52d2ac87c59006292d1de92ef6
vanshika <vanshikamehra131@gmail.com>
2025-07-30 12:37:22 +0530

    add lab question

---
+++ Phase2-Conditionals/Composite Patterns/Quesn.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Quesn {
+    public static void main(String[] args) {
+        
+    
+     Scanner sc = new Scanner(System.in);
+        System.out.printf("Enter n: ");
+        int n = sc.nextInt();
+
+        
+
+
+        for(int i =1;i<=n;i++){
+            for(int j=1;j<i;j++){
+                System.out.print( " " + " ");
+            }
+            for(int j=1;j<=n+1-i;j++){
+                System.out.print(j+" ");
+            }
+            for(int j =n-i ; j>=1;j--){
+                System.out.print(j+" ");
+
+            }
+
+            System.out.println();
+
+        }
+        sc.close();  
+    
+}
+}
