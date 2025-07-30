03e3b30df344876907a5973533a85186c81cf078
vanshika <vanshikamehra131@gmail.com>
2025-07-30 12:36:34 +0530

    add Number Bridge

---
+++ Phase2-Conditionals/Composite Patterns/NumberBridge.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class NumberBridge {
+     public static void main(String[] args) {
+        
+    
+     Scanner sc = new Scanner(System.in);
+        System.out.printf("Enter n: ");
+        int n = sc.nextInt();
+
+       for(int i=1;i<=2*n-1;i++){
+        System.out.print( i + " ");
+
+       }
+         System.out.println();
+         n--;
+
+
+
+        for(int i =1;i<=n;i++){ //rows
+             int a=1;
+            for(int j=1;j<=n+1-i;j++){ //numbers
+                System.out.print( a++ + " ");
+            }
+            for(int j=1;j<=2*i-1;j++){ //spaces
+                System.out.print( " " + " ");
+                a++;
+            }
+            for(int j=5; j<=n+5-i;j++){ //numbers 
+                System.out.print( a++ + " ");
+            }
+            System.out.println();
+        
+        }
+        sc.close();  
+    
+    
+}
+
+
+    
+}
