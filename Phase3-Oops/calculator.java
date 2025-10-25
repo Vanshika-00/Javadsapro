60bba0e66f4a552306e4aea506c57e92656bc19c
Vanshika Mehra <vanshikamehra131@gmail.com>
2025-10-25 21:31:48 +0530

    add calculator

---
+++ Phase3-Oops/calculator.java
@@ -0,0 +1,15 @@
+class calc{
+    public void add (int a , int b){
+        int result=a+b;
+        System.out.println("Result is : "+result);
+    }
+}
+
+
+public class calculator {
+    public static void main (String [] arg ){
+        calc c1=new calc();
+        c1.add(5,4);
+    }
+    
+}
