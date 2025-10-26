548106716cf1f6749e928d89dec2786ba7b0f5e5
Vanshika Mehra <vanshikamehra131@gmail.com>
2025-10-26 13:42:53 +0530

    add static var

---
+++ Phase3-Oops/staticvar.java
@@ -0,0 +1,10 @@
+class LoanApp{
+    static float rateOfInterest=9.5f;
+}
+
+public class staticvar {
+    public static void main (String [] args){
+        System.out.println(LoanApp.rateOfInterest);
+    }
+    
+}
