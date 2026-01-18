3e9493010f9a1502524a35bf8bee36e3e6762949
Vanshika Mehra <vanshikamehra131@gmail.com>
2026-01-18 20:05:07 +0530

    add output of array

---
+++ Phase4-Arrays/Basics/outputOfArray.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class outputOfArray {
+
+    public static void main(String[] args) {
+        
+    
+
+    Scanner sc= new Scanner(System.in);
+
+    System.out.println("Enter array size:");
+    int n=sc.nextInt();
+
+    int[] arr= new int[n];
+
+
+    //input loop
+    for(int i=0;i<n;i++){
+        arr[i] = sc.nextInt();
+    }
+   //output loop
+
+    for(int i=0;i<n;i++){
+        System.out.print(arr[i]+" ");
+    }
+
+    sc.close();
+
+    
+}
+}
