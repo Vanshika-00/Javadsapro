0a1e9d0a51e3a8e2a2e57663a8fc77e7baf3c86d
Vanshika Mehra <vanshikamehra131@gmail.com>
2026-01-18 13:58:56 +0530

    add declaration and initialisation

---
+++ Phase4-Arrays/Basics/basicSyntax.java
@@ -0,0 +1,27 @@
+public class basicSyntax{
+    public static void main(String[] args) {
+        
+        // Initialising individual elements of an array
+        int[] arr = new int[5];
+        arr[0]=10;
+        arr[1]=20;
+        arr[2]=30;
+        arr[3]=40;
+        arr[4]=50;
+
+        
+
+        //Output of array elements
+        System.out.print(arr[0]+" ");
+        System.out.print(arr[1]+" ");
+        System.out.print(arr[2]+" ");
+        System.out.print(arr[3]+" ");
+        System.out.print(arr[4]+" ");
+
+        arr[0]=99;
+        arr[0]+=10;
+        System.out.println("\nUpdated first element: " + arr[0]);
+
+
+    }
+}
