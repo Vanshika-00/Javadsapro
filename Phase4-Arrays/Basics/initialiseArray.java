b8ae4005d3443603c2134de73f7119168bcbff7a
Vanshika Mehra <vanshikamehra131@gmail.com>
2026-01-18 20:04:51 +0530

    add initialisation sec way

---
+++ Phase4-Arrays/Basics/initialiseArray.java
@@ -0,0 +1,13 @@
+public class initialiseArray {
+
+    public static void main(String[] args) {
+        int[] arr ={ 10,20,30,40,50,4,5,6,7,9};
+        int n = arr.length;
+        System.out.println(n);
+
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    
+}
