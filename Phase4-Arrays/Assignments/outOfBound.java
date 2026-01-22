e401fb4f73e853d32eba8a1830c08856dc944564
Vanshika Mehra <vanshikamehra131@gmail.com>
2026-01-22 20:34:34 +0530

    add out of bond

---
+++ Phase4-Arrays/Assignments/outOfBound.java
@@ -0,0 +1,12 @@
+public class outOfBound {
+
+    public static void main(String[] args) {
+        
+
+         int[] arr= {12,45,78,34,23,89,90};
+         arr[10]=100; // This line will cause ArrayIndexOutOfBoundsException
+    }
+
+   
+    
+}
