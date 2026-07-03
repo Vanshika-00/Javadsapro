cdd7ff13f17d741f7d4b0248c7a85e7d0b781632
Vanshika Mehra <vanshikamehra131@gmail.com>
2026-07-03 20:32:46 +0530

    Assignment Ques

---
+++ Phase4-Arrays/Assignments/changeIndices.java
@@ -0,0 +1,25 @@
+
+/* Q Given an array of integers, change the value of all odd indexed elements to its second multiple and
+increment all even indexed values by 10.*/
+
+public class changeIndices {
+
+    public static void main(String[] args) {
+
+    int[] arr = { 1, 2, 3, 4, 5 };
+
+    for(int i =0;i<arr.length;i++){
+        if(i%2==0){
+            arr[i]= arr[i]+10;
+        }
+        else{
+            arr[i]= arr[i] *2;
+        }
+    }
+
+    for(int i =0;i<arr.length;i++){
+        System.out.print(arr[i]+" ");
+    }
+    
+}
+}
