668eb642bd6e799921215a9bece0aecf70d40fc1
Vanshika Mehra <vanshikamehra131@gmail.com>
2026-07-07 05:07:02 +0530

    Add basic Declaration

---
+++ Phase4-Arrays/twoDimArray/basicDeclaration.java
@@ -0,0 +1,16 @@
+package twoDimArray;
+
+public class basicDeclaration {
+
+    
+
+    public static void main(String[] args) {
+
+        int [][] grid = new int [3][4]; // 3 rows and 4 columns
+        grid[2][3] = 5; // Assigning value to the last element of the 2D array
+
+        System.out.println(grid[2][3]);
+        System.out.println(grid[0][0]);
+    
+}
+}
