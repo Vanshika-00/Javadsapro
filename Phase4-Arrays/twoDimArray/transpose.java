5f76d72bb2a29236adb84f6f1879cf2104149407
Vanshika Mehra <vanshikamehra131@gmail.com>
2026-07-11 14:17:42 +0530

    Add transpose

--- Phase4-Arrays/twoDimArray/transpose.java
+++ Phase4-Arrays/twoDimArray/transpose.java
@@ -15,15 +15,12 @@ public class transpose {
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 transpose[i][j] = arr[j][i];
-            }
-        }
-
-         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                System.out.print(transpose[i][j] + " ");
+                 System.out.print(transpose[i][j] + " ");
             }
             System.out.println();
         }
+
+        
 
         
 }
