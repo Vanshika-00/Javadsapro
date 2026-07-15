4abdfb08acae64a12b5813f797b1b060cf228ae4
Vanshika Mehra <vanshikamehra131@gmail.com>
2026-07-15 22:56:43 +0530

    add built-In String Methods

---
+++ Phase4-Arrays/Strings/builtInStringMethods.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class builtInStringMethods {
+    public static void main(String[] args){
+
+        String s= "Vanshika Mehra";
+        System.out.println(s.length()); // length of string
+        System.out.println(s.charAt(4)); // character at index 4
+    }
+    
+}
