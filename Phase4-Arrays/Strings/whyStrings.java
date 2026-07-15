78a47123e5377dbbcd2fc00300f0cb4754eaca78
Vanshika Mehra <vanshikamehra131@gmail.com>
2026-07-15 22:59:46 +0530

    why string

---
+++ Phase4-Arrays/Strings/whyStrings.java
@@ -0,0 +1,20 @@
+package Strings;
+
+public class whyStrings{
+
+    public static void main(String[] args) {
+        
+        //Vanshu
+
+    //     char [] arr = {'V','a','n','s','h','u'};
+    //     for(int i=0;i<arr.length;i++){
+    //     System.out.print(arr[i]+" ");
+    // }
+
+    String x;
+    x = "Vanshu";
+    System.out.println(x);
+
+    }
+    
+}
