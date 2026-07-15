c4344eb2c2df87c2ec4614a4d5c79908631960e5
Vanshika Mehra <vanshikamehra131@gmail.com>
2026-07-15 22:58:07 +0530

    add lower upper case

---
+++ Phase4-Arrays/Strings/lowerUpperCase.java
@@ -0,0 +1,26 @@
+package Strings;
+
+public class lowerUpperCase {
+
+    public static void main(String[] args){
+
+        String s = "Vanshika Is Angel";
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+
+        System.out.println(s);
+
+        s=s.toUpperCase();
+        System.out.println(s);
+
+        String a="abc";
+        String b="xyz";
+
+        System.out.println(a.concat(b));
+        System.out.println(a);
+        a=a.concat(b);
+        System.out.println(a);
+
+    }
+    
+}
