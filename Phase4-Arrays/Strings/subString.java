da8b7066d42a445b89e1c4017495bb78a1280d09
Vanshika Mehra <vanshikamehra131@gmail.com>
2026-07-15 22:59:26 +0530

    add subString

---
+++ Phase4-Arrays/Strings/subString.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class subString {
+
+    public static void main(String[] args){
+
+        String s = "Vanshika";
+        System.out.println(s.substring(2));
+        System.out.println(s.substring(2,5));
+
+    }
+    
+}
