e2bf419b88fc08a81403b88e2b2d9091d62a06e7
Vanshika Mehra <vanshikamehra131@gmail.com>
2026-07-15 22:56:23 +0530

    add str

---
+++ Phase4-Arrays/Strings/indexOf.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class indexOf {
+    public static void main(String[] args){
+
+        String s = "Raghav Garg";
+        System.out.println(s.indexOf('a'));
+        System.out.println(s.indexOf('r'));
+        System.out.println(s.lastIndexOf('a'));
+
+        System.out.println("Apple".compareTo("Banana")); // -1
+        System.out.println("Banana".compareTo("Apple")) ; // 1    
+    }
+    
+}
