cde59f82be40da26d79dc890bc1c2737acd783a3
Vanshika Mehra <vanshikamehra131@gmail.com>
2026-07-16 21:23:16 +0530

    Add new Java files

---
+++ Phase4-Arrays/Strings/noOfDigit.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class noOfDigit {
+
+    public static void main(String[] args) {
+        int a= 12345;
+
+        String b=Integer.toString(a);
+
+        System.out.println(b.length());
+    }
+    
+}
---
+++ Phase4-Arrays/Strings/plusOperator.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class plusOperator {
+
+    public static void main(String[] args){
+
+        System.out.println("abc"+10+20);
+
+        System.out.println(10+20+"abc");
+
+
+    }
+    
+}
