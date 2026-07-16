d485aec03748635b8656053de257fb5e48b08a42
Vanshika Mehra <vanshikamehra131@gmail.com>
2026-07-16 21:22:48 +0530

    Add new Java files

---
+++ Phase4-Arrays/Strings/convertIntoStr.java
@@ -0,0 +1,18 @@
+package Strings;
+import java.util.Scanner;
+
+public class convertIntoStr {
+    public static void main(String[] args) {
+        
+    
+
+    Scanner sc= new Scanner (System.in);
+
+    int n= sc.nextInt();
+    String s= ""+n;
+
+    System.out.println(s);
+
+    sc.close();
+}
+}
