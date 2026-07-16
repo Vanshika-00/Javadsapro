29c201a602fe109f877076f02986dc3358e5fab7
Vanshika Mehra <vanshikamehra131@gmail.com>
2026-07-16 21:23:06 +0530

    Add new Java files

---
+++ Phase4-Arrays/Strings/interning.java
@@ -0,0 +1,18 @@
+package Strings;
+
+public class interning {
+
+    public static void main(String[] args) {
+        
+        String s = "Raghav";
+        //Raghav=Madhav
+        // s.charAt(0)=M
+        // s.charAt(2)=d
+
+        s="Madhav";
+
+        System.err.println(s);
+
+    }
+    
+}
