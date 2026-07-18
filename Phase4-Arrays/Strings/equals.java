48942663c5e3d3584c2fe891e21df5f7381d3e60
Vanshika Mehra <vanshikamehra131@gmail.com>
2026-07-18 18:57:22 +0530

    add file

---
+++ Phase4-Arrays/Strings/equals.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class equals {
+
+    public static void main(String[] args) {
+        String s = "abcxyz";
+        String t="abc";
+        t+="xyz";
+
+        System.out.println(s==t);
+    }
+    
+}
